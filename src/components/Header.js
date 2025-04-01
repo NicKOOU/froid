53812c5251b175510739e2383d90209f04e24567
Nicolas Kourban <nicolas.kourban@epita.fr>
2025-04-01 23:46:28 +0200

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <header className="header">
+        <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
             <Link to="/" className="logo">冷库安装专家</Link>
             <nav>
                 <ul>
+                    <li><Link to="/">首页</Link></li>
                     <li><Link to="/about">关于我们</Link></li>
                     <li><Link to="/services">服务</Link></li>
                     <li><Link to="/projects">项目案例</Link></li>
