3baa5c3b7ad7d375a4170ba9957980db30e6f5dd
Nicolas Kourban <nicolas.kourban@epita.fr>
2025-04-11 21:33:58 +0200

    change page to top and animation fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -8,11 +8,37 @@ import Services from './components/Services';
 import Contact from './components/Contact';
 import './styles/App.scss';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Attendre que le DOM soit complètement chargé
+    const handleLoad = () => {
+      setIsLoaded(true);
+    };
+
+    if (document.readyState === 'complete') {
+      setIsLoaded(true);
+    } else {
+      window.addEventListener('load', handleLoad);
+      return () => window.removeEventListener('load', handleLoad);
+    }
+  }, []);
+
   return (
     <Router>
-      <div className="App">
-
+      <ScrollToTop />
+      <div className={`App ${isLoaded ? 'loaded' : ''}`}>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,7 +47,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
-
       </div>
     </Router>
   );
